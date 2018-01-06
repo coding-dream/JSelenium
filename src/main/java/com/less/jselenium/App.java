a087ffb6c66afc386d31a17148e49b0ee53f1a23
coding-dream <352085768@qq.com>
2018-01-06 17:16:22 +0800

    phantomjs_example

--- src/main/java/com/less/jselenium/App.java
+++ src/main/java/com/less/jselenium/App.java
@@ -1,7 +1,22 @@
 package com.less.jselenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("hello world");
+        testChrome();
+}
+
+    private static void testChrome() {
+        String driverPath = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver",driverPath + File.separator + "JSelenium" + File.separator + "drivers" +File.separator + "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://need88.com");
+        driver.navigate().refresh();
+        driver.quit();
     }
 }
