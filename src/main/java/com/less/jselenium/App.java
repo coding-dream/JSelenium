4edbad5027afa4de2b4bae601bebf85d88607518
coding-dream <352085768@qq.com>
2018-02-24 18:41:34 +0800

    update

--- src/main/java/com/less/jselenium/App.java
+++ src/main/java/com/less/jselenium/App.java
@@ -1,28 +1,32 @@
 package com.less.jselenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-//        testChrome();
+    public static void main(String[] args) throws InterruptedException {
+        testChrome();
         testPhantomjs();
 }
 
     private static void testPhantomjs() {
-        String url = "https://www.jianshu.com/";
+        String url = "https://m.tianyancha.com/search?key=北京爱奇艺科技有限公司&checkFrom=searchBox";
         PhantomJSDriver webDriver = (PhantomJSDriver) renderingJS();
         webDriver.get(url);
         String pageSource = webDriver.getPageSource();
-//		Document document = Jsoup.parse(pageSource);
-        System.out.println(pageSource);
+        WebElement webElement = webDriver.findElement(By.xpath("//div[@class='new-border-bottom pt5 pb5 ml15 mr15'][1]//a[1]"));
+        System.out.println(webElement.getAttribute("href"));
+		// Document document = Jsoup.parse(pageSource);
         webDriver.quit();
     }
 
@@ -32,40 +36,29 @@ public class App {
         String phantomjsPath = System.getProperty("user.dir") + File.separator + "JSelenium" + File.separator + "drivers" +File.separator + "phantomjs.exe";
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setJavascriptEnabled(true);
-        caps.phantomjs().setBrowserName("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36");
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, phantomjsPath);
 
-		/*
-		 * webMagic中知道的一些参数，通用参数（可选）
-		 *
-		// Disable "web-security", enable all possible "ssl-protocols" and
-		// "ignore-ssl-errors" for PhantomJSDriver
-		// sCaps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new
-		// String[] {
-		// "--web-security=false",
-		// "--ssl-protocol=any",
-		// "--ignore-ssl-errors=true"
-		// });
-
-			ArrayList<String> cliArgsCap = new ArrayList<String>();
-			cliArgsCap.add("--web-security=false");
-			cliArgsCap.add("--ssl-protocol=any");
-			cliArgsCap.add("--ignore-ssl-errors=true");
-			caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,cliArgsCap);
-			caps.setBrowserName("phantomjs");
-		 */
-
+        List<String> cliArgsCap = new ArrayList<String>();
+        cliArgsCap.add("--web-security=false");
+        cliArgsCap.add("--ssl-protocol=any");
+        cliArgsCap.add("--ignore-ssl-errors=true");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
+        caps.setBrowserName("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36");
         PhantomJSDriver webDriver = new PhantomJSDriver(caps);
-        webDriver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().pageLoadTimeout(3000, TimeUnit.SECONDS);
         return webDriver;
     }
 
-    private static void testChrome() {
+    private static void testChrome() throws InterruptedException {
         String driverPath = System.getProperty("user.dir");
         System.setProperty("webdriver.chrome.driver",driverPath + File.separator + "JSelenium" + File.separator + "drivers" +File.separator + "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.jianshu.com/");
-        driver.navigate().refresh();
+        driver.get("https://m.tianyancha.com/search?key=北京爱奇艺科技有限公司&checkFrom=searchBox");
+        String pageSource = driver.getPageSource();
+        WebElement webElement = driver.findElement(By.xpath("//div[@class='new-border-bottom pt5 pb5 ml15 mr15'][1]//a[1]"));
+        System.out.println(webElement.getAttribute("href"));
+        Thread.sleep(3000);
+        // driver.navigate().refresh();
         driver.quit();
     }
 }
